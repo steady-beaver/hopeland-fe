81cc32916a6d686188c57f280c37c13795a35e67
steady-beaver <ivaylo.yordanov.petrov@gmail.com>
2024-05-04 16:40:12 +0300

    Build fixes

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,17 @@
 import Link from 'next/link';
-
-import Container from '@/components/Container';
 import Layout from '@/components/Layout';
-import Section from '@/components/Section';
 
-import styles from 'styles/pages/Error.module.scss';
+import styles from '@/styles/pages/Error.module.scss';
 
 export default function Custom404() {
   return (
     <Layout>
-      <Section>
-        <Container className={styles.center}>
-          <h1>Page Not Found</h1>
-          <p className={styles.errorCode}>404</p>
-          <p className={styles.errorMessage}>The page you were looking for could not be found.</p>
-          <p>
-            <Link href="/">Go back home</Link>
-          </p>
-        </Container>
-      </Section>
+      <h1>Page Not Found</h1>
+      <p className={styles.errorCode}>404</p>
+      <p className={styles.errorMessage}>The page you were looking for could not be found.</p>
+      <p>
+        <Link href="/">Go back home</Link>
+      </p>
     </Layout>
   );
 }
--- src/pages/500.js
+++ src/pages/500.js
@@ -1,27 +1,20 @@
 import Link from 'next/link';
-
-import Container from '@/components/Container';
 import Layout from '@/components/Layout';
-import Section from '@/components/Section';
 
-import styles from 'styles/pages/Error.module.scss';
+import styles from '@/styles/pages/Error.module.scss';
 
 export default function Custom500() {
   return (
     <Layout>
-      <Section>
-        <Container className={styles.center}>
-          <h1>Internal Error</h1>
-          <p className={styles.errorCode}>500</p>
-          <p className={styles.errorMessage}>
-            Uh oh, something went wrong. Please try refreshing the page or clearing site data. If the problem persists,
-            reach out to let us know!
-          </p>
-          <p>
-            <Link href="/">Go back home</Link>
-          </p>
-        </Container>
-      </Section>
+      <h1>Internal Error</h1>
+      <p className={styles.errorCode}>500</p>
+      <p className={styles.errorMessage}>
+        Uh oh, something went wrong. Please try refreshing the page or clearing site data. If the problem persists,
+        reach out to let us know!
+      </p>
+      <p>
+        <Link href="/">Go back home</Link>
+      </p>
     </Layout>
   );
 }
