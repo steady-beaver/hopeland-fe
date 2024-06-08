4cff8ad4d6e9a320b4e273a7cbc118648e919ccb
steady-beaver <ivaylo.yordanov.petrov@gmail.com>
2024-06-08 13:47:39 +0300

    Google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,32 @@
 import '@/styles/globals.scss';
 import { SnackbarProvider } from 'notistack';
+import Script from 'next/script';
 
 function App({ Component, pageProps = {} }) {
   return (
-    <SnackbarProvider maxSnack={3}>
-      <Component {...pageProps} />
-    </SnackbarProvider>
+    <div>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-N06W4EKKHK`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-N06W4EKKHK');
+          `,
+        }}
+      />
+      {/* Google Analytics */}
+      <SnackbarProvider maxSnack={3}>
+        <Component {...pageProps} />
+      </SnackbarProvider>
+    </div>
   )
 }
 
